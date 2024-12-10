9998ba680c59dc8c02b6b1be0233e12f4bb88b54
kanchanasklm <s180227@rguktsklm.ac.in>
2024-12-10 13:45:42 +0530

    create a express server

--- src/app.js
+++ src/app.js
@@ -3,16 +3,18 @@ const express = require("express");
 const app = express();
 
 app.use("/test",(req,res)=>{
-    console.log("hello from the server");
+    res.send("hi from server");
 });
 
-app.use("/",(req,res)=>{
-    console.log("hello hello hello");
-});
+//app.use("/",(req,res)=>{
+    //console.log("hello hello hello");
+//
 
-app.use((req,res)=>{
-    console.log("hello from the server");
-});
-app.listen(3000 ,()=>{
+//});
+
+//app.use((req,res)=>{
+    //res.send("hello from the server");
+//});
+app.listen(2024,()=>{
     console.log("server is successfully listening on port 3000...");
 });
