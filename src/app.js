6174e6fe816a3f782ecad4c42e048154e10a052b
kanchanasklm <s180227@rguktsklm.ac.in>
2024-12-10 23:07:20 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -2,18 +2,9 @@ const express = require("express");
 
 const app = express();
 
-
-
-app.get("/user",(req,res)=>{
-    res.send({firstname:"Bindupriya",lastname:"Samanthula"});
-});
-
-app.post("/user",(req,res)=>{
-    res.send("Data successfully saved in database");
-});
-
-app.delete("/user",(req,res)=>{
-    res.send("Deleted successfully!");
+app.get("/user/:userid/:name/:password",(req,res)=>{
+    console.log(req.params);
+    res.send("namaste nodejs");
 });
 
 app.listen(3000,()=>{
