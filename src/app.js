03c0e742a9fe5f63b917e797cee7cdfeaa305bbb
kanchanasklm <s180227@rguktsklm.ac.in>
2024-12-11 12:34:16 +0530

    multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -2,10 +2,22 @@ const express = require("express");
 
 const app = express();
 
-app.get("/user/:userid/:name/:password",(req,res)=>{
-    console.log(req.params);
-    res.send("namaste nodejs");
+app.get("/user",[(req,res,next)=>{
+    console.log("handling the route user");
+    next();
+    res.send("1st response");
+   
+},(req,res,next)=>{
+    console.log("handling the route user2");
+    res.send("2nd response");
+    next();
+}],(req,res,next)=>{
+    console.log("handling the route user3");
+    res.send("3nd response");
+    next();
 });
+
+
 
 app.listen(3000,()=>{
     console.log("server is successfully listening on port 3000...");
