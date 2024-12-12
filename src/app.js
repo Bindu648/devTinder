8c7d2289c13bebb217bb701c6596a2cb5d231e58
kanchanasklm <s180227@rguktsklm.ac.in>
2024-12-12 12:40:58 +0530

    dynamic data to signup

--- src/app.js
+++ src/app.js
@@ -2,13 +2,11 @@ const express = require("express");
 const connectDB=require("./config/database");
 const app = express();
 const User=require("./models/user");
+app.use(express.json());
+
 app.post("/signup",async(req,res)=>{
-    const user=new User({
-        firstName:"Manvithsai",
-        lastName:"korada",
-        emailId:"sai@gmail.com",
-        password:"sai123",
-    });
+    //creating a new instance of the User model
+    const user=new User(req.body);
     try{
         await user.save();
         res.send("user added successfully");
